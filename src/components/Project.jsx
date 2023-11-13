acf92b443ef071dad3a267a7820cf7927595db01
sentuh <142236429+sentuhxd@users.noreply.github.com>
2023-11-13 12:36:18 -0500

    updated resume and project images

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
-import img1 from '../assets/pwatexteditor.png';
-import img2 from '../assets/image1.png';
-import img3 from '../assets/image2.png';
+import img1 from '../assets/texteditor.png';
 import img4 from '../assets/awesomeartifacts.png';
+import img5 from '../assets/ecommerce.png';
+import img6 from '../assets/gameboardio.png';
 import Aos from 'aos';
 
 const Projects = () => {
@@ -13,9 +13,9 @@ const Projects = () => {
        <div id="Projects" className="p-20 flex flex-col items-center justify-center">
         <h1 data-aos="fade-right" className="text-[52px] font-semibold mb-20 leading-normal uppercase text-fuchsia-500">Projects</h1>
         <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 justify-around gap-10">
-            <img data-aos="fade-up" height={250} width={250} className="flex items-center justify-center rounded-3xl p-1 border-2 border-fuchsia-800 b_glow" src={img1} alt="" />
-            <img data-aos="fade-down" height={250} width={250} className="flex items-center justify-center rounded-3xl p-1 border-2 border-fuchsia-800 b_glow" src={img2} alt="" />
-            <img data-aos="fade-up" height={250} width={250} className="flex items-center justify-center rounded-3xl p-1 border-2 border-fuchsia-800 b_glow" src={img3} alt="" />
+            <img data-aos="fade-up" height={250} width={250} className="flex items-center justify-center rounded-3xl p-1 border-2 border-fuchsia-800 b_glow" src={img1} alt="Text Editor PWA" />
+            <img data-aos="fade-down" height={250} width={250} className="flex items-center justify-center rounded-3xl p-1 border-2 border-fuchsia-800 b_glow" src={img5} alt="ecom and stripe functionality" />
+            <img data-aos="fade-up" height={250} width={250} className="flex items-center justify-center rounded-3xl p-1 border-2 border-fuchsia-800 b_glow" src={img6} alt="gameboardio" />
             <img data-aos="fade-down" height={250} width={250} className="flex items-center justify-center rounded-3xl p-1 border-2 border-fuchsia-800 b_glow" src={img4} alt="" />
         </div>
        </div>
